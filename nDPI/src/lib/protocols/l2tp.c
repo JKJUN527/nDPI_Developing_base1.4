d898232553d35a4e17da6b06ada5e65063e782cf
jkjun <631642753@qq.com>
2017-12-29 00:57:46 -0800

    修复l2tp协议

--- nDPI/src/lib/protocols/l2tp.c
+++ nDPI/src/lib/protocols/l2tp.c
@@ -3,11 +3,13 @@
 void ndpi_search_l2tp(struct ndpi_detection_module_struct *ndpi_struct,struct ndpi_flow_struct *flow)
 {
 	struct ndpi_packet_struct *packet = &flow->packet;
+    //NDPI_LOG(NDPI_PROTOCOL_RTP, ndpi_struct, NDPI_LOG_DEBUG, "dport is :%x.\n",ntohs(packet->udp->dest));
+   // NDPI_LOG(NDPI_PROTOCOL_RTP, ndpi_struct, NDPI_LOG_DEBUG, "dport is :%x.\n",ntohs(packet->udp->source));
 
     if((packet->payload_packet_len>= 6)
 		&& ((packet->payload[1]&0xff) == 0x02)
 		&& ((packet->payload[0]&0x34) == 0)
-		&& (packet->udp != NULL && (packet->udp->dest==1701 ||packet->udp->source==1701))//默认端口为1701，jkjun添加
+		&& (packet->udp != NULL && (ntohs(packet->udp->dest)==1701 ||ntohs(packet->udp->source)==1701))//默认端口为1701，jkjun添加
 	){
     NDPI_LOG(NDPI_PROTOCOL_RTP, ndpi_struct, NDPI_LOG_DEBUG, "Found l2tp.\n");
     ndpi_int_add_connection(ndpi_struct, flow, NDPI_PROTOCOL_L2TP, NDPI_REAL_PROTOCOL);	
